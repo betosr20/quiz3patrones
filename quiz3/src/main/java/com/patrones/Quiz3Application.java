b9f3354743b1e6086d04d137c3b05b9e8b00e197
betosr20 <betog19@gmail.com>
2018-02-09 15:15:54 -0600

    creacion del proyecto de STS

---
+++ quiz3/src/main/java/com/patrones/Quiz3Application.java
@@ -0,0 +1,12 @@
+package com.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz3Application.class, args);
+	}
+}
