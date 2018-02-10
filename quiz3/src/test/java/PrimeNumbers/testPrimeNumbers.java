9c4c2a7b29c7ec7a2460e1ee86f9ef9d8b86e462
betosr20 <betog19@gmail.com>
2018-02-10 13:24:11 -0600

    Se crearon los paquetes correspondientes a las nuevas clases

---
+++ quiz3/src/test/java/PrimeNumbers/testPrimeNumbers.java
@@ -0,0 +1,17 @@
+package PrimeNumbers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testPrimeNumbers {
+
+	@Test
+	public void test() {
+		primeNumbers primes = new PrimeNumbers();
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
