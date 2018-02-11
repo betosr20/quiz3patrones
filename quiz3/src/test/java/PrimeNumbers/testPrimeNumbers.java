6e5dec07f7ffe5a6bad9f05abae79b00676e8636
betosr20 <betog19@gmail.com>
2018-02-10 18:25:46 -0600

    Se hace refactor de las pruebas agregando un @Before que permite limpiar la lista y asi usar la misma lista en todos los casos de prueba

--- quiz3/src/test/java/PrimeNumbers/testPrimeNumbers.java
+++ quiz3/src/test/java/PrimeNumbers/testPrimeNumbers.java
@@ -5,33 +5,37 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class testPrimeNumbers {
 	primeNumbers primes = new primeNumbers();
+	List<Integer> testList = new ArrayList<Integer>();
+	
+	@Before
+	public void limpiarLista() {
+		testList.clear();
+	}
+	
 	@Test
 	public void test() {
-		List<Integer> testList = new ArrayList<Integer>();
 		assertEquals(testList, primes.calcPrimes(1));
 	}
 	
 	@Test
 	public void test2() {
-		List<Integer> testList = new ArrayList<Integer>();
 		testList.add(2);
 		assertEquals(testList, primes.calcPrimes(2));
 	}
 
 	@Test
 	public void test3() {
-		List<Integer> testList = new ArrayList<Integer>();
 		testList.add(3);
 		assertEquals(testList, primes.calcPrimes(3));
 	}
 	
 	@Test
 	public void test4() {
-		List<Integer> testList = new ArrayList<Integer>();
 		testList.add(2);
 		testList.add(2);
 		assertEquals(testList, primes.calcPrimes(4));
@@ -39,9 +43,8 @@ public class testPrimeNumbers {
 
 	@Test
 	public void test5() {
-		List<Integer> testList2 = new ArrayList<Integer>();
-		testList2.add(5);
-		assertEquals(testList2, primes.calcPrimes(5));
+		testList.add(5);
+		assertEquals(testList, primes.calcPrimes(5));
 	}
 
 }
